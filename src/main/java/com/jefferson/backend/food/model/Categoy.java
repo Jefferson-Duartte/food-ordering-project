3b5265d83b7e97dcba16d9e344e7521a918d31bf
Jefferson Duarte <duartejefferson123@gmail.com>
2024-06-24 18:52:27 -0300

    feat: create Category entity

---
+++ src/main/java/com/jefferson/backend/food/model/Categoy.java
@@ -0,0 +1,25 @@
+package com.jefferson.backend.food.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+}
