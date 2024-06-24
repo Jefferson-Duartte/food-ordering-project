d6ea37eabf64fcb76d2c5026d35974af95aad9f4
Jefferson Duarte <duartejefferson123@gmail.com>
2024-06-24 18:51:20 -0300

    feat: create RestaurantDto

---
+++ src/main/java/com/jefferson/backend/food/dto/RestaurantDto.java
@@ -0,0 +1,24 @@
+package com.jefferson.backend.food.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    @ElementCollection
+    private List<String> images;
+
+    private String description;
+
+}
